d7ce1a0181b4001f6c7cac3d6cd1ebb18b4f4b91
Ben Lodge <benrlodge@gmail.com>
2014-09-27 12:41:38 -0400

    add demo

--- README.md
+++ README.md
@@ -2,35 +2,16 @@
 
 Ever need to split up a url to get some info? Say the subdomain, or the top level domain or what have you or what not? I did, so I tossed this module together.
 
-I used the revealing module pattern, so it's as simple as...
+[See a demo](http://benrlodge.github.io/domain-info/)
 
+It's as simple as...
 
-```javascript 
-
-Domain = (function(){
-	
-	var url = function(){
-		return document.URL; //e.g. http://www.example.org:8888/foo/bar?q=baz#bangbang
-	}
-
-	var protocol = function(){
-		return window.location.protocol; // e.g. http:
-	}
-	
-	var host = function(){
-		return window.location.host;  // include port number if specified e.g. example.org:8888
-	}
-
-	...
-
-
+```javascript
 
 Domain.url();
 
 Domain.protocol();
 
 Domain.and-so-on-and-so-forth
-
-
 
 ```
--- js/domain.js
+++ js/domain.js
@@ -1,45 +1,50 @@
+/*
+Domain-Info.js
+By Ben Lodge
+See more at benrlodge.com
+ */
 
 Domain = (function(){
 
     var url = function(){
         return document.URL; //e.g. http://www.example.org:8888/foo/bar?q=baz#bangbang
-    }
+    };
 
     var protocol = function(){
         return window.location.protocol; // e.g. http:
-    }
+    };
 
     var host = function(){
         return window.location.host;  // include port number if specified e.g. example.org:8888
-    }
+    };
 
     var hostName = function(){
         return window.location.hostname; // eg. example.org
-    }
+    };
 
     var pathName = function(){
         return window.location.pathname; // eg. /foo/bar
-    }
+    };
 
     var parts = function(){
         return (url()).split("//");
-    }
+    };
 
     var sub = function(){
         var p = parts();
         if (!p){ return false; }
         return p[1].split('.');
-    }
+    };
 
     var topLevelDomain = function(){
         var s = sub();
         if(!s[2]){ return false; }
         return s[2].split('/')[0]; // top level domain
-    }
+    };
 
     var subdomain = function(){
         return sub[0];
-    }
+    };
 
     return {
         url: url,
@@ -49,6 +54,6 @@ Domain = (function(){
         hostName: hostName,
         topLevelDomain: topLevelDomain,
         pathName: pathName
-    }
+    };
 
 })();
